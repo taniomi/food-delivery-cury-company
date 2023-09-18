b74eab94dc8636f88e803c8ce4bcc7499b17d3b0
taniomi <131834608+taniomi@users.noreply.github.com>
2023-09-18 13:27:05 -0300

    Modified cury-company.py with Deliverer View

--- cury-company.py
+++ cury-company.py
@@ -46,7 +46,7 @@ df['Week_Year'] = df['Order_Date'].dt.strftime('%U')
 # 2 Streamlit                                                        #
 #====================================================================#
 # 2.1 Main Page
-st.header('Cury Co. Ltd.')
+st.header('Cury Co. Ltd. - Deliverer View')
 st.write('Fast and savory food! Curry is the best food!😋')
 
 # 2.2 Sidebar
@@ -69,56 +69,86 @@ df = df.loc[filtered_lines,:]
 st.sidebar.markdown('Powered by Taniomi')
 
 # 2.3 Tabs
-tab1, tab2, tab3 = st.tabs(['Management View', 'Tactical View', 'Map'])
+tab1, tab2, tab3 = st.tabs(['Management View', '_', '_'])
 
 # 2.3.1 Management View
 with tab1:
     with st.container():
-        orders_day = (df.loc[:,['ID','Order_Date']]
-                      .groupby('Order_Date')['ID']
-                      .count().reset_index())
-        chart_day = px.bar(orders_day,
-                           title='Orders per day',
-                           x='Order_Date',y='ID',
-                           height=350)
-        st.plotly_chart(chart_day)
+        st.title('Overall Metrics')
+        col1, col2, col3, col4 = st.columns(4, gap='large')
+        
+        with col1:
+            oldest = df['Delivery_person_Age'].max()
+            col1.metric('Oldest', oldest)
+
+        with col2:
+            youngest = df['Delivery_person_Age'].min()
+            col2.metric('Youngest', youngest)
+
+        with col3:
+            best_vehicle_cond = df['Vehicle_condition'].max()
+            col3.metric('Best vehicle', best_vehicle_cond)
+
+        with col4:
+            worst_vehicle_cond = df['Vehicle_condition'].min()
+            col4.metric('Worst vehicle', worst_vehicle_cond)
 
     with st.container():
+        st.markdown("""---""")
+        st.title('Ratings')
         col1, col2 = st.columns(2)
 
         with col1:
-            entregas_por_trafego = (df.loc[:,['ID','Road_traffic_density','City']]
-                                    .groupby(['Road_traffic_density','City'])
-                                    .count().reset_index())
-            entregas_por_trafego = (entregas_por_trafego.loc[
-                                    entregas_por_trafego.loc[:,'Road_traffic_density'] != 'NaN',
-                                    :])
-            entregas_por_trafego['Order_%'] = entregas_por_trafego['ID'] / entregas_por_trafego['ID'].sum()
-
-            chart_traffic = px.pie(entregas_por_trafego,
-                                   title='Orders by traffic (%)',
-                                   values='Order_%',names='Road_traffic_density',
-                                   height=350,width=350)
-            st.plotly_chart(chart_traffic)
+            st.subheader('Mean ratings by deliverer')
+            mean_rating_by_deliv = (df.loc[:,['Delivery_person_ID', 'Delivery_person_Ratings']]
+                                      .groupby('Delivery_person_ID')
+                                      .mean()
+                                      .reset_index())
+            st.dataframe(mean_rating_by_deliv)
 
         with col2:
-            entregas_por_city_e_traffic = (df.loc[:,['ID','City','Road_traffic_density']]
-                                           .groupby(['City','Road_traffic_density'])
-                                           .count().reset_index())
-            entregas_por_city_e_traffic = (entregas_por_city_e_traffic.loc[
-                                           entregas_por_city_e_traffic.loc[:,'City'] != 'NaN',
-                                           :])
-            entregas_por_city_e_traffic = (entregas_por_city_e_traffic.loc[
-                                           entregas_por_city_e_traffic.loc[:,'Road_traffic_density'] != 'NaN',
-                                           :])
+            st.subheader('Mean ratings by traffic conditions')
+            mean_rating_by_traffic = (df.loc[:,['Delivery_person_Ratings','Road_traffic_density']]
+                                        .groupby('Road_traffic_density')
+                                        .agg({'Delivery_person_Ratings': ['mean', 'std']}))
+            mean_rating_by_traffic.columns = ['delivery_mean', 'delivery_std']
+            mean_rating_by_traffic.reset_index()
+            st.dataframe(mean_rating_by_traffic)
 
-            chart_city_traffic = px.bar(entregas_por_city_e_traffic,
-                                        title='Orders by city and traffic',
-                                        x='City',y='ID',
-                                        height=350,width=350,
-                                        color='Road_traffic_density')
+            st.subheader('Mean ratings by climate conditions')
+            mean_rating_by_climate = (df.loc[:,['Delivery_person_Ratings','Weatherconditions']]
+                                        .groupby('Weatherconditions')
+                                        .agg({'Delivery_person_Ratings': ['mean', 'std']}))
+            mean_rating_by_climate.columns = ['delivery_mean', 'delivery_std']
+            mean_rating_by_climate.reset_index()
+            st.dataframe(mean_rating_by_climate)
 
-            st.plotly_chart(chart_city_traffic,use_container_witdh=True)
+    with st.container():
+        st.markdown("""---""")
+        st.title('Delivery speed')
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader('Fastest deliverers')
+            dfaux = (df.loc[:,['Delivery_person_ID', 'City', 'Time_taken(min)']]
+                       .groupby(['City', 'Delivery_person_ID']).min()
+                       .sort_values(['City', 'Time_taken(min)']).reset_index())
+            dfaux1 = dfaux.loc[dfaux['City'] == 'Metropolitian', :].head(10)
+            dfaux2 = dfaux.loc[dfaux['City'] == 'Semi-Urban', :].head(10)
+            dfaux3 = dfaux.loc[dfaux['City'] == 'Urban', :].head(10)
+            fastest_deliverers = pd.concat([dfaux1, dfaux2, dfaux3]).reset_index(drop=True)
+            st.dataframe(fastest_deliverers)
+ 
+        with col2:
+            st.subheader('Slowest deliverers')
+            dfaux = (df.loc[:,['Delivery_person_ID', 'City', 'Time_taken(min)']]
+                       .groupby(['City', 'Delivery_person_ID']).max()
+                       .sort_values(['City', 'Time_taken(min)'], ascending=False).reset_index())
+            dfaux1 = dfaux.loc[dfaux['City'] == 'Metropolitian', :].head(10)
+            dfaux2 = dfaux.loc[dfaux['City'] == 'Semi-Urban', :].head(10)
+            dfaux3 = dfaux.loc[dfaux['City'] == 'Urban', :].head(10)
+            slowest_deliverers = pd.concat([dfaux1, dfaux2, dfaux3]).reset_index(drop=True)
+            st.dataframe(slowest_deliverers)
 
 # 2.3.2 Tactical View
 with tab2:
@@ -151,7 +181,7 @@ with tab2:
 
         st.plotly_chart(chart_deliverer_wk)
 
-# 2.3.3 Map
+# 2.3.4 Map
 with tab3:
     city_location_traffic = (df.loc[:,['City','Road_traffic_density','Delivery_location_latitude','Delivery_location_longitude']]
                              .groupby(['City','Road_traffic_density'])
